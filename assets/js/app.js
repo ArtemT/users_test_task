7394312b82f85afc3af870fcbf51ac4276146200
Artem Timofeev <temahome@gmail.com>
2019-01-23 08:30:13 +0300

    Frontend prepared for filtering. Minor cleanup.

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,10 +16,10 @@ $(document).ready(function() {
     });
 
     const sortable = $(".sortable");
-    const link = '<a class="sort-link"></a>';
+    const sortLink = '<a class="sort-link"></a>';
     sortable.not(".sorted").hover(
         function() {
-            $(link).attr('href', "/user/sorted-by/" + this.dataset.field)
+            $(sortLink).attr('href', "/user/sorted-by/" + this.dataset.field)
                 .appendTo(this);
         },
         function() {
@@ -28,7 +28,7 @@ $(document).ready(function() {
     );
     sortable.filter(".sorted").hover(
         function() {
-            $(link).attr('href', "/user/sorted-by/" + this.dataset.field + "/desc")
+            $(sortLink).attr('href', "/user/sorted-by/" + this.dataset.field + "/desc")
                 .appendTo(this);
         },
         function() {
@@ -36,8 +36,41 @@ $(document).ready(function() {
         }
     );
 
-    const filterable = $(".filterable");
-    filterable.not(".filtered").click(function() {
-        console.log(this);
-    });
+    const options = {
+        title: "Test",
+        placement: "top",
+        template: '<div class="popover" role="tooltip"><div class="arrow"></div><div class="popover-body"></div></div>',
+        html: true,
+        content: function() {
+            const field = this.parentNode.dataset.field;
+            var out = '<div class="input-group input-group-sm filter-' + field + '">';
+            if (field == 'status') {
+                out = out + '<select class="form-control filter-' + field + '-val">';
+                $.each({ 1: "Первый", 2: "Второй", 3: "Третий" }, function(i, title) {
+                    out = out + '<option value="' + i + '">' + title + '</option>';
+                });
+                out = out + '</select>';
+            }
+            else {
+                out = out + '<input type="text" class="form-control filter-' + field + '-val">';
+            }
+             out = out + '<div class="input-group-append">'
+                 + '<button class="btn btn-outline-secondary" type="button">Найти</button>'
+                 + '</div></div>';
+            return out;
+        },
+        trigger: 'click focus',
+    }
+    $(".filterable").popover(options)
+        .on('show.bs.popover', function() {
+            $(".filterable").popover('hide').removeClass("filtered");
+            $(this).addClass("filtered");
+        })
+        .on('shown.bs.popover', function() {
+            const field = this.parentNode.dataset.field;
+            $(".filter-" + field + " button").click(function() {
+                const val = $(".filter-" + field + "-val").val();
+            });
+        });
+
 });
